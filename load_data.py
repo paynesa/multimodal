957135fc6947cfcbaccde5a0861c238e70d95d64
paynesa <47441354+paynesa@users.noreply.github.com>
2019-02-08 18:33:04 -0500

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,8 +1,18 @@
-"""
-Purpose:
- - Collect image embeddings in one txt file, convert to Magnitude in command line 
- - Create the training set (x_train, y_train)
-"""
+#Purpose:
+# - Collect image embeddings in one txt file, convert to Magnitude in command line 
+# - Create the training set (x_train, y_train)
+
+
+
+'''
+Arg vecs: 
+avg or not (default = don't average?) 
+folders /data1/minh/data
+data_path /data1/minh/multimodal/img_embedding.txt
+'''
+
+
+
 
 import numpy as np
 import pickle
@@ -10,8 +20,44 @@ import pandas as pd
 import os
 from pymagnitude import *
 
+
+#create one image embedding for each word by average pooling all image feature vectors
+#@save img_embedding: a numpy array of image embeddings
+def create_image_embedding_avg():
+    # read the files that contain words and their image embeddings
+    # TODO: handle duplicates or not (maybe Magnitude will eventually handle this? 
+
+    folders = os.listdir('/data1/minh/data')
+    for f in folders:
+        print("Folder name: {}".format(f))
+        words = pd.read_csv('/data1/minh/data/'+f, sep=' ', header=None).values
+        print("Done loading from pandas")
+        
+        start = 0
+        for i in range(words.shape[0]-1):
+
+            # only process English words, which start with 'row'
+            if words[i][0] != words[i+1][0]:
+                end = i+1
+                data_path = '/data1/minh/multimodal/img_embedding.txt'
+                img_embedding = words[start:end,1:]
+
+                # average pooling to create one single image embedding
+                average_embedding = img_embedding.sum(axis=0) / img_embedding.shape[0]
+                average_embedding = np.insert(average_embedding, 0, words[i][0])
+
+                # save all embeddings to txt, convert txt to magnitude in cmd line 
+                with open(data_path, 'a') as f:
+                    np.savetxt(f, average_embedding.reshape(1, average_embedding.shape[0]), fmt="%s")
+                start = i+1
+            
+            if 'column-' in words[i+1][0]:
+                print("Number of English words: {}".format(i/10))
+                break
+    print("Done average pooling")
+
+
 def create_image_embedding():
-   #we need to try without average-pooling 
   
   """
     create one image embedding for each word by average pooling all image feature vectors
@@ -30,20 +76,28 @@ def create_image_embedding():
             # only process English words, which start with 'row'
             if words[i][0] != words[i+1][0]:
                 end = i+1
-                data_path = '/data1/minh/multimodal/img_embedding.txt'
+                data_path = '/data1/minh/multimodal/img_embedding_noav.txt'
                 img_embedding = words[start:end,1:]
-                # average pooling to create one single image embedding
-                average_embedding = img_embedding.sum(axis=0) / img_embedding.shape[0]
-                average_embedding = np.insert(average_embedding, 0, words[i][0])
                 # save all embeddings to txt, convert txt to magnitude in cmd line 
                 with open(data_path, 'a') as f:
-                    np.savetxt(f, average_embedding.reshape(1, average_embedding.shape[0]), fmt="%s")
+                    np.savetxt(f, img_embedding.reshape(1, img_embedding.shape[0]), fmt="%s")
                 start = i+1
             
             if 'column-' in words[i+1][0]:
                 print("Number of English words: {}".format(i/10))
                 break
-    print("Done average pooling")
+    print("Done")
+
+
+
+'''
+Arg vecs: 
+avg or not (default = don't average?) 
+folders /data1/minh/data
+data_path /data1/minh/multimodal/img_embedding.txt
+'''
+
+
 
 def create_train_set():
     """
