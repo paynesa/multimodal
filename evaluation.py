28fcf7539f4b750525b227d0fc6ba983bf3cee73
Sarah Payne <47441354+paynesa@users.noreply.github.com>
2019-06-13 11:27:24 -0400

    Argument parsing

--- evaluation.py
+++ evaluation.py
@@ -1,152 +1,142 @@
-"""
-Purpose:
-- Evaluate multimodal embeddings against 6 word similarity tests
-"""
+import os, pickle, subprocess
 from process_eval_set import get_eval_set_list
 from argparse import ArgumentParser
-
+from scipy import stats
+from pymagnitude import *
 import numpy as np
 import pandas as pd
-from scipy import stats
-import os
-from pymagnitude import *
-import pickle 
 
-def convert_dict_to_txt(path):
-    """
-    convert the dictionary that contains all predicted embeddings into txt format 
-    """
-    with open(path+'_all.p', 'rb') as fp:
-        word_dict = pickle.load(fp)
+l = ["Wordsim_sim", "Wordsim_rel", "Simlex", "MEN", "SemSim", "VisSim"]
 
-    for k, v in word_dict.items():
-        v = v.astype('<U100')
-        v = np.insert(v, 0, k)
-        with open(path+'_all.txt', 'a') as f:
-            np.savetxt(f, v.reshape(1, v.shape[0]), fmt='%s')
+#convert the dictionary that contains all predicted embeddings into magnitude format
+def convert_dict_to_magnitude(path):
+	with open(path+'.p', 'rb') as fp:
+		word_dict = pickle.load(fp)
+	for k, v in word_dict.items():
+		v = v.astype('<U100')
+		v = np.insert(v, 0, k)
+		with open(path+'temp.txt', 'a') as f:
+			np.savetxt(f, v.reshape(1, v.shape[0]), fmt='%s')
+	i = path+'temp.txt'
+	o = path+'.magnitude'
+	cmd = ("python3.6 -m pymagnitude.converter -i "+i+" -o "+o).split()
+	try:
+		subprocess.check_output(cmd)
+		print("Magnitude conversion successful")
+	except:
+		raise Exception("Problem converting to magnitude, please convert from the command line")
 
-def compute_pair_sim(word1, word2): 
-    """
-    compute cosine similarity between two words
-    """
-    dot_product = np.dot(word1, word2)
-    length_word1 = np.linalg.norm(word1)
-    length_word2 = np.linalg.norm(word2)
-    return dot_product/(length_word1 * length_word2)
 
+#compute the cosine similarity between two words
+def compute_pair_sim(word1, word2):
+	dot_product = np.dot(word1, word2)
+	mag_word1 = np.linalg.norm(word1)
+	mag_word2 = np.linalg.norm(word2)
+	return (dot_product/(mag_word1 * mag_word2))
+
+
+#compute similarity for all words in the evaluation set
 def compute_sim(eval_set, word_dict):
-    """
-    compute similarity for all words in the evaluation set
-    @param word_dict: dictionary: keys: words, values: learned embeddings
-    @param eval_set_type: type of evaluation set (zs/vis) 
-    @return a numpy array of word similarity
-    """ 
-    word_sim = []
-    for i in range(eval_set.shape[0]):
-        embedding1 = word_dict[eval_set[i][0]]
-        embedding2 = word_dict[eval_set[i][1]]
-        pair_sim = compute_pair_sim(embedding1, embedding2)
-        word_sim.append(pair_sim)
-    word_sim = np.asarray(word_sim)
-    
-    return word_sim
+	word_sim = []
+	for i in range(eval_set.shape[0]):
+		embedding1 = word_dict[eval_set[i][0]]
+		embedding2 = word_dict[eval_set[i][1]]
+		pair_sim = compute_pair_sim(embedding1, embedding2)
+		word_sim.append(pair_sim)
+	word_sim = np.asarray(word_sim)
+	return word_sim
 
+
+#compute similarity for all words in the evaluation set when in magnitude
 def compute_sim_magnitude(eval_set, word_dict):
-    """
-    compute similarity for all words in the evaluation set
-    @param word_dict: dictionary: keys: words, values: learned embeddings
-    @param eval_set_type: type of evaluation set (zs/vis) 
-    @return a numpy array of word similarity
-    """ 
-    word_sim = []
-    for i in range(eval_set.shape[0]):
-        embedding1 = word_dict.query(eval_set[i][0])
-        embedding2 = word_dict.query(eval_set[i][1])
-        pair_sim = compute_pair_sim(embedding1, embedding2)
-        word_sim.append(pair_sim)
-    word_sim = np.asarray(word_sim)
-    
-    return word_sim
+	word_sim = []
+	for i in range(eval_set.shape[0]):
+		embedding1 = word_dict.query(eval_set[i][0])
+		embedding2 = word_dict.query(eval_set[i][1])
+		pair_sim = compute_pair_sim(embedding1, embedding2)
+		word_sim.append(pair_sim)
+	word_sim = np.asarray(word_sim)
+	return word_sim
 
+
+#Print out evaluation results (correlation, P-value) for all sets, either of type ZS or VIS 
 def evaluate(eval_set_type, word_dict, dict_format):
-    """
-    Print out evaluation results (correlation, P-value) for all sets, either of type ZS or VIS 
-    @param eval_set_type: Type of eval set (ZS/VIS)
-    @param word_dict: corresponding dictionary: keys: zs/vis words, values: predicted embeddings 
-    """ 
-    path = '/data1/minh/evaluation/'
-    for i in range(6):
-        eval_set = pd.read_csv(path+str(i)+'_'+eval_set_type+'.txt', sep=' ', header=None).as_matrix()
-        if dict_format == 'dict':
-            model_sim = compute_sim(eval_set, word_dict)
-        else:
-            model_sim = compute_sim_magnitude(eval_set, word_dict)
-        cor, pval = stats.spearmanr(model_sim, eval_set[:,2])
-        print("Correlation for {} ({}): {:.3f}, P-value: {:.3f}".format(str(i), eval_set_type, cor, pval))
-    print()
+	path = '/data1/minh/evaluation/'
+	for i in range(6):
+		eval_set = pd.read_csv(path+str(i)+'_'+eval_set_type+'.txt', sep=' ', header=None).as_matrix()
+		if (dict_format == "dict"):
+			model_sim = compute_sim(eval_set, word_dict)
+		else:
+			model_sim = compute_sim_magnitude(eval_set, word_dict)
+		cor, pval = stats.spearmanr(model_sim, eval_set[:,2])
+		print("Correlation for {} ({}): {:.3f}, P-value: {:.3f}".format(str(l[i]), eval_set_type, cor, pval))
+	print()
 
+
+#Print out evaluation results (correlation, P-value) for all sets (full set)
 def evaluate_all(eval_set_list, word_dict, dict_format):
-    """
-    Print out evaluation results (correlation, P-value) for all sets (full set)
-    @param eval_set_list: List of eval sets (in matrix form)
-    @param word_dict: corresponding dictionary: keys: all words, values: predicted embeddings 
-    @param dict_format: format of dictionary, 'dict' (normal dictionary) or 'magnitude' (magnitude object)
-    """  
-    for i in range(len(eval_set_list)):
-        if dict_format == 'dict':
-            model_sim = compute_sim(eval_set_list[i], word_dict)
-        else:
-            model_sim = compute_sim_magnitude(eval_set_list[i], word_dict)
-        cor, pval = stats.spearmanr(model_sim, eval_set_list[i][:,2])
-        print("Correlation for {} (all): {:.3f}, P-value: {:.3f}".format(str(i), cor, pval))
-    print() 
+	for i in range(len(eval_set_list)):
+		if dict_format == 'dict':
+			model_sim = compute_sim(eval_set_list[i], word_dict)
+		else:
+			model_sim = compute_sim_magnitude(eval_set_list[i], word_dict)
+		cor, pval = stats.spearmanr(model_sim, eval_set_list[i][:,2])
+		print("Correlation for {} (all): {:.3f}, P-value: {:.3f}".format(str(l[i]), cor, pval))
+	print()
 
 def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument('model', default='normal', type=str, help='[normal, c_linear, c_neural]')
-    parser.add_argument('path', type=str, help='path to dictionary that contains predicted embeddings')
-    args = parser.parse_args()
-    return args
+	parser = ArgumentParser()
+	parser.add_argument('model', default='normal', type=str, help='[normal, c_linear, c_neural]')
+	parser.add_argument('path', type=str, help='path to dictionary that contains predicted embeddings')
+	parser.add_argument('--w', type=str, help='path to word vectors if evaluating a concatenated model')
+	args = parser.parse_args()
+	return args
 
 def main():
-    args = parse_args()
-    # load evaluation sets
-    eval_set_list = get_eval_set_list()
+	#parse the arguments and make sure that they are valid
+	args = parse_args()
+	if (args.path == None):
+		raise Exception("You must input a valid path to your predicted embeddings")
+	if (args.model != "c_linear") and (args.model != "c_neural") and (args.model != "normal"):
+		raise Exception("Invalid evaluation type")
 
-    # open dictionaries that contains predicted embeddings
-    with open(args.path+"_vis.p", 'rb') as fp:
-        word_dict_vis = pickle.load(fp)
-    with open(args.path+"_zs.p", 'rb') as fp:
-        word_dict_zs = pickle.load(fp)
-    with open(args.path+"_all.p", 'rb') as fp:
-        word_dict_all = pickle.load(fp)
-    
-    #args.m: linear, neural, c_linear, c_neural
-    # evaluate a normal model (not concatenated)
-    if args.model == 'normal':
-        evaluate('vis', word_dict_vis, 'dict')
-        evaluate('zs', word_dict_zs, 'dict')
-        evaluate_all(eval_set_list, word_dict_all, 'dict')
-    # evaluate a concatenated model
-    else:    
-        word_dict = Magnitude('/data1/embeddings/pymagnitude/word.magnitude')
-        
-        if args.model == 'c_linear':        
-            pred_dict = Magnitude('/data1/embeddings/pymagnitude/predicted_linear.magnitude')
-        else:        
-            pred_dict = Magnitude('/data1/embeddings/pymagnitude/predicted_neural.magnitude')
-        # concatenate a model with glove 
-        fused_dict = Magnitude(word_dict, pred_dict)
-        print('Dimension of concatenated vectors: {}'.format(fused_dict.dim))
+	#load the evaluation sets
+	print("Loading evaluation sets...")
+	eval_set_list = get_eval_set_list()
+	print("Done loading evaluation sets")
+	
+	#evaluate a normal model (not concatenated)
+	if (args.model == "normal"):
+		print("Loading predictions...")
+		try:
+			with open(args.path+"_vis.p", 'rb') as fp:
+				word_dict_vis = pickle.load(fp)
+			with open(args.path+"_zs.p", 'rb') as fp:
+				word_dict_zs = pickle.load(fp)
+			with open(args.path+"_all.p", 'rb') as fp:
+				word_dict_all = pickle.load(fp)
+			print("Predictions loaded")
+		except:
+			raise Exception("There was a problem loading your predictions. Please check the filepath")
+		evaluate('vis', word_dict_vis, 'dict')
+		evaluate('zs', word_dict_zs, 'dict')
+		evaluate_all(eval_set_list, word_dict_all, 'dict')
 
-        evaluate('vis', fused_dict, 'magnitude')
-        evaluate('zs', fused_dict, 'magnitude')
-        evaluate_all(eval_set_list, fused_dict, 'magnitude')
+	#evaluate concatenated model
+	else:
+		print("Converting to magnitude")
+		convert_dict_to_magnitude(args.path+'_all')
+		try:
+			print("Concatenating vectors...")
+			pred_dict = Magnitude(args.path+'_all.magnitude')
+			word_dict = Magnitude(args.w)
+			fused_dict = Magnitude(word_dict, pred_dict)
+		except:
+			raise Exception("There was a problem loading your word embeddings. Please check the filepath")
+		print("Dimension of concatenated vectors: {}".format(fused_dict.dim))
+		evaluate('vis', fused_dict, 'magnitude')
+		evaluate('zs', fused_dict, 'magnitude')
+		evaluate_all(eval_set_list, fused_dict, 'magnitude')
 
 if __name__ == '__main__':
-    # convert dictionary to txt file, then convert to Magnitude format in command line 
-    # comment out main() if this function is called()
-    # args = parse_args()
-    # convert_dict_to_txt()
-    main()
-
+	main()
